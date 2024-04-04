7608f41e3ce42e5e1c5ed4b0efc6661125fb76f3
Halfsa <szendefyalex@gmail.com>
2024-04-04 14:17:44 +0200

    ValidateToken és LoginPost fájlok hozzáadása

---
+++ src/ValidateToken.tsx
@@ -0,0 +1,28 @@
+import {jwtDecode} from 'jwt-decode'
+import axios from "axios";
+import {useEffect, useState} from "react";
+function ValidateToken(){
+    const token = localStorage.getItem('access_token');
+    const refresh = localStorage.getItem('refresh_token');
+    const [returnedToken,setReturnedToken] = useState(token);
+    useEffect(() => {
+        if (token !== null){
+        const decodedToken = jwtDecode(token);
+        if (decodedToken.exp!- Date.now()*0.001 <=5){
+            axios.post('auth/refresh', {}, {
+                headers: {
+                    Authorization: `Bearer ${refresh}`
+                }
+            }).then(res => {
+                console.log(res.data.message);
+                localStorage.setItem('access_token', res.data.access_token)
+                localStorage.setItem('refresh_token', res.data.refresh_token)
+                setReturnedToken(res.data.access_token)
+            }).catch(e => {
+                console.log(e)
+            })
+        }}
+    }, );
+    return returnedToken;
+}
+export default ValidateToken;
